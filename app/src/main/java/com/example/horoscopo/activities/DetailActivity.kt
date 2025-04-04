032eba2942f29701033a78c8df5e711552bd0aaa
RafaelLibrero <librerorafael@outlook.com>
2025-04-04 19:17:15 +0200

    Getting data from API (no retrofit)

--- app/src/main/java/com/example/horoscopo/activities/DetailActivity.kt
+++ app/src/main/java/com/example/horoscopo/activities/DetailActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -14,6 +15,16 @@ import com.example.horoscopo.R
 import com.example.horoscopo.data.Horoscope
 import com.example.horoscopo.data.HoroscopeProvider
 import com.example.horoscopo.utils.SessionManager
+import com.google.android.material.progressindicator.LinearProgressIndicator
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class DetailActivity : AppCompatActivity() {
 
@@ -21,6 +32,8 @@ class DetailActivity : AppCompatActivity() {
     private lateinit var tvId: TextView
     private lateinit var tvDates: TextView
 
+    private lateinit var horoscopeLuckTextView: TextView
+    private lateinit var progressBar: LinearProgressIndicator
     private lateinit var horoscope: Horoscope
 
     private lateinit var session: SessionManager
@@ -89,16 +102,57 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
+    private fun getHoroscopeLuck() {
+        progressBar.visibility = View.VISIBLE
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val url =
+                URL("https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily?sign=${horoscope.id}")
+            val urlConnection = url.openConnection() as HttpsURLConnection
+            urlConnection.requestMethod = "GET"
+
+            try {
+                if (urlConnection.responseCode == HttpURLConnection.HTTP_OK) {
+
+                    val bufferedReader =
+                        BufferedReader(InputStreamReader(urlConnection.inputStream))
+                    val response = StringBuffer()
+                    var inputLine: String?
+
+                    while ((bufferedReader.readLine().also { inputLine = it }) != null) {
+                        response.append(inputLine)
+                    }
+                    bufferedReader.close()
+
+                    val result = JSONObject(response.toString()).getJSONObject("data")
+                        .getString("horoscope_data")
+
+                    CoroutineScope(Dispatchers.Main).launch {
+                        progressBar.visibility = View.GONE
+                        horoscopeLuckTextView.text = result
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                urlConnection.disconnect()
+            }
+        }
+    }
+
     private fun initComponents() {
         ivIcon = findViewById(R.id.ivIcon)
         tvId = findViewById(R.id.tvID)
         tvDates = findViewById(R.id.tvDates)
+        horoscopeLuckTextView = findViewById(R.id.horoscopeLuckTextView)
+        progressBar = findViewById(R.id.progressBar)
     }
 
     private fun render() {
         ivIcon.setImageResource(horoscope.icon)
         tvId.setText(horoscope.name)
         tvDates.setText(horoscope.dates)
+        getHoroscopeLuck()
     }
 
     private fun setFavoriteIcon() {
